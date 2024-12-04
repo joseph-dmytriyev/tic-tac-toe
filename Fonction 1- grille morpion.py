4759f77c4920ef096225056d72fe2e0dc67d55b2
icham.djilali-merzoug <icham.djilali-merzoug@laplateforme.io>
2024-12-04 10:32:54 +0100

    modification des noms de variables et variables abstraites(for)

--- Fonction 1- grille morpion.py
+++ Fonction 1- grille morpion.py
@@ -4,17 +4,17 @@ Created on Wed Dec  4 09:43:09 2024
 
 @author: josep
 """
-
+#Board creation
 tab = [[" " for _ in range(3)] for _ in range(3)]
 
 # Function to display the tic-tac-toe board
 def board_morp(tab):
-    n = len(tab)  # Size of the board (3)
+    length_tab = len(tab)  # Size of the board (3)
     # Printing the top border line
     print("+" + "---+" * n)
-    for i in range(3):  # Loop through the rows of the board
-        print(f"| {tab[i][0]} | {tab[i][1]} | {tab[i][2]} |")  # Display each cell
-        if i < 2:  # Horizontal separator lines between rows
+    for rows in range(3):  # Loop through the rows of the board
+        print(f"| {tab[rows][0]} | {tab[rows][1]} | {tab[rows][2]} |")  # Display each cell
+        if rows < 2:  # Horizontal separator lines between rows
             print("|---+---+---|")
     # Printing the bottom border line
-    print("+" + "---+" * n)
+    print("+" + "---+" * length_tab)
